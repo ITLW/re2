2312a9bb9ba1cee612c90b07c7529528deefe7d4
李伟 <LW@adeMacBook-Pro.local>
2015-11-24 14:26:03 +0800

    Initial Commit

---
+++ GItDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GItDemo
+//
+//  Created by 李伟 on 15/11/24.
+//  Copyright (c) 2015年 a. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GItDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GItDemo
+//
+//  Created by 李伟 on 15/11/24.
+//  Copyright (c) 2015年 a. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
