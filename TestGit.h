71115b5709864f4430760e33a13a16cac09a2cd6
李伟 <LW@adeMacBook-Pro.local>
2015-11-25 11:39:29 +0800

    添加TestGit

---
+++ TestGit.h
@@ -0,0 +1,13 @@
+//
+//  TestGit.h
+//  GItDemo
+//
+//  Created by 李伟 on 15/11/25.
+//  Copyright (c) 2015年 a. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestGit : UIViewController
+
+@end
---
+++ TestGit.m
@@ -0,0 +1,38 @@
+//
+//  TestGit.m
+//  GItDemo
+//
+//  Created by 李伟 on 15/11/25.
+//  Copyright (c) 2015年 a. All rights reserved.
+//
+
+#import "TestGit.h"
+
+@interface TestGit ()
+@property(nonatomic,strong)NSString *name;
+@end
+
+@implementation TestGit
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"TestGit");
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
